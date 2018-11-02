177fd63e261b06edf26e3717674de03ab8bf5b1d
王蕾 <867550109@qq.com>
2018-11-02 20:49:36 +0800

    Change
    Change

--- calculate/ViewController.swift
+++ calculate/ViewController.swift
@@ -9,7 +9,80 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var result: UITextField!
+    var temp:Double = 0
+    var flag = 0
+    @IBAction func botton0(_ sender: Any) {
+        result.text = result.text! + "0"
+    }
+    @IBAction func botton1(_ sender: Any) {
+        result.text = result.text! + "1"
+    }
+    @IBAction func botton2(_ sender: Any) {
+        result.text = result.text! + "2"
+    }
+    @IBAction func botton3(_ sender: Any) {
+        result.text = result.text! + "3"
+    }
+    @IBAction func botton4(_ sender: Any) {
+        result.text = result.text! + "4"
+    }
+    @IBAction func botton5(_ sender: Any) {
+        result.text = result.text! + "5"
+    }
+    @IBAction func botton6(_ sender: Any) {
+        result.text = result.text! + "6"
+    }
+    @IBAction func botton7(_ sender: Any) {
+        result.text = result.text! + "7"
+    }
+    @IBAction func botton8(_ sender: Any) {
+        result.text = result.text! + "8"
+    }
+    @IBAction func botton9(_ sender: Any) {
+        result.text = result.text! + "9"
+    }
+    @IBAction func dot(_ sender: Any) {
+        result.text = result.text! + "."
+    }
+    @IBAction func bottonAC(_ sender: Any) {
+        result.text = ""
+    }
+    @IBAction func add(_ sender: Any) {
+        temp = Double(result.text!)!
+        result.text = ""
+        flag = 1
+    }
+    @IBAction func minus(_ sender: Any) {
+        temp = Double(result.text!)!
+        result.text = ""
+        flag = 2
+    }
+    @IBAction func mutily(_ sender: Any) {
+        temp = Double(result.text!)!
+        result.text = ""
+        flag = 3
+    }
+    @IBAction func divide(_ sender: Any) {
+        temp = Double(result.text!)!
+        result.text = ""
+        flag = 4
+    }
+    @IBAction func results(_ sender: Any) {
+        if flag == 1{
+            temp = temp + Double(result.text!)!
+        }
+        if flag == 2{
+            temp = temp - Double(result.text!)!
+        }
+        if flag == 3{
+            temp = temp * Double(result.text!)!
+        }
+        if flag == 4{
+            temp = temp / Double(result.text!)!
+        }
+        result.text = "\(temp)"
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -17,4 +90,3 @@ class ViewController: UIViewController {
 
 
 }
-
